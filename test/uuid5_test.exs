c19c442bd4f55b36591f5d999ec8468cfeb6afff
Alexander Malaev <am@malaev.net>
2026-09-10 23:16:25 +0300

    Add a test suite
    The library had no tests. These cover what the module does today, so that a
    change to it can be seen.

---
+++ test/uuid5_test.exs
@@ -0,0 +1,53 @@
+defmodule UUID5Test do
+  use ExUnit.Case, async: true
+
+  doctest UUID5
+
+  @uuid "6ba7b810-9dad-11d1-80b4-00c04fd430c8"
+
+  describe "cast/1" do
+    test "accepts a uuid" do
+      assert {:ok, @uuid} == UUID5.cast(@uuid)
+    end
+
+    test "accepts a uuid in upper case, unchanged" do
+      upper = String.upcase(@uuid)
+
+      assert {:ok, upper} == UUID5.cast(upper)
+    end
+
+    test "accepts what generate/0 produces" do
+      uuid = UUID5.generate()
+
+      assert {:ok, uuid} == UUID5.cast(uuid)
+    end
+
+    test "refuses a value of the wrong length" do
+      assert :error == UUID5.cast("6ba7b810-9dad-11d1-80b4")
+    end
+
+    test "refuses a term that is not a binary" do
+      assert :error == UUID5.cast(nil)
+      assert :error == UUID5.cast(42)
+    end
+  end
+
+  describe "dump/1" do
+    test "converts a uuid to sixteen bytes" do
+      assert {:ok, binary} = UUID5.dump(@uuid)
+      assert byte_size(binary) == 16
+    end
+  end
+
+  describe "load/1" do
+    test "converts sixteen bytes back to a uuid" do
+      {:ok, binary} = UUID5.dump(@uuid)
+
+      assert {:ok, @uuid} == UUID5.load(binary)
+    end
+
+    test "refuses anything else" do
+      assert :error == UUID5.load(<<1, 2, 3>>)
+    end
+  end
+end
